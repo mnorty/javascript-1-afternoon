18fa17d7d136a89a4c67d87d0ae6e37f3f91f439
mnorty <mnorty@gmail.com>
2019-04-16 13:44:59 -0600

    First 6  answers

--- practice.js
+++ practice.js
@@ -1,32 +1,32 @@
 //////////////////PROBLEM 1////////////////////
 
-//Create a variable called myName that is a string data type, change 1
+//Create a variable called myName that is a string data type
 
-//Code here
+let myName = "Mike"
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
-//Code here
+/let myAge = 28
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
-//Code here
+let lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
-//Code here
+let greatestFear = undefined
 
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
-//Code here
+devMountainGoal = null
 
 //////////////////PROBLEM 6////////////////////
 
@@ -35,7 +35,9 @@
 //greeting should return the string "Hello, "
 //plus the value of the name parameter.
 
-//Code here
+function greeting(name) {
+  return "Hello, " + name
+}
 
 
 //////////////////PROBLEM 7////////////////////
