6e632d211877dde31447579d158d8098432967f3
mnorty <mnorty@gmail.com>
2019-04-16 14:23:52 -0600

    Almost done

--- practice.js
+++ practice.js
@@ -99,13 +99,14 @@ function nameCheck(name) {
 // that will be numbers.
 // The add function should return the two parameters added together
 
-//Code Here
+function add(num1,num2) {
+  return num1 + num2
+}
 
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
-//Code Here
-
+let mathSum = add(3,4)
 //////////////////PROBLEM 12////////////////////
 
 //Write a function called faveColorFinder that takes in one parameter called color
@@ -115,7 +116,16 @@ function nameCheck(name) {
 // If the passed in color equals 'black', return 'so trendy'
 // Otherwise, you should return the string 'you need to evaluate your favorite color choice'
 
-// Code here
+function faveColorFinder(color) {
+  if (color === "red")
+    return "red is a great color"
+  if (color === "green")
+    return "green is a solid favorite color"
+  if (color = "black")
+    return "so trendy"
+  else 
+    return "You need to evaluate your favorite color choice"
+}
 
 //////////////////PROBLEM 13////////////////////
 
@@ -139,28 +149,29 @@ function pond() {
 //as strings.
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let globalScope = ["duck"];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathroomScope = ["duck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathtubScope = ["duck", "sailorDuck", "rubberDuck",];
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let pondScope = ["duck", "realDuck"];
 
 //////////////////PROBLEM 14////////////////////
 
 //Create a variable called age with your age assigned to you
 
-// Code Here
+let age = 28 
+
 
 // FLASH FORWARD TO NEXT YEAR
 // reassign the value of age to be one greater than it was, because, we all get older
 
-// Code Here
+age = age++
 
 // Good news! We can live forever. Set your age to 999
 
-// Code Here
+age = 999
